1de6c49781dea7917293b2f7d0a20092bc4bbfe6
steyr109 <125200802+steyr109@users.noreply.github.com>
2023-06-03 04:35:07 +0300

    Add files via upload

--- blackjack.js
+++ blackjack.js
@@ -1,38 +1,39 @@
-let dealerSum = 0;
-let yourSum = 0;
+var dealerSum = 0;
+var yourSum = 0;
 
-let dealerAceCount = 0;
-let yourAceCount = 0; 
+var dealerAceCount = 0;
+var yourAceCount = 0; 
 
-let hidden;
-let deck;
+var hidden;
+var deck;
 
-let canHit = true; 
+var canHit = true;
 
-window.onload = () => {
+window.onload = function() {
     buildDeck();
     shuffleDeck();
     startGame();
 }
 
 function buildDeck() {
-    const values = "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K";
-    const types = "C", "D", "H", "S";
-    deck = ;
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
 
     for (let i = 0; i < types.length; i++) {
         for (let j = 0; j < values.length; j++) {
-            deck.push(valuesj + "-" + typesi);
+            deck.push(values[j] + "-" + types[i]); 
         }
     }
+
 }
 
 function shuffleDeck() {
     for (let i = 0; i < deck.length; i++) {
-        const j = Math.floor(Math.random()  deck.length); // (0-1)  52 => (0-51.9999)
-        const temp = decki;
-        decki = deckj;
-        deckj = temp;
+        let j = Math.floor(Math.random() * deck.length); 
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
     }
     console.log(deck);
 }
@@ -41,10 +42,13 @@ function startGame() {
     hidden = deck.pop();
     dealerSum += getValue(hidden);
     dealerAceCount += checkAce(hidden);
+
+
     while (dealerSum < 17) {
-        const cardImg = document.createElement("img");
-        const card = deck.pop();
-        cardImg.src = ./cards/${card}.png;
+
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
         dealerSum += getValue(card);
         dealerAceCount += checkAce(card);
         document.getElementById("dealer-cards").append(cardImg);
@@ -52,9 +56,9 @@ function startGame() {
     console.log(dealerSum);
 
     for (let i = 0; i < 2; i++) {
-        const cardImg = document.createElement("img");
-        const card = deck.pop();
-        cardImg.src = ./cards/${card}.png;
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
         yourSum += getValue(card);
         yourAceCount += checkAce(card);
         document.getElementById("your-cards").append(cardImg);
@@ -63,7 +67,7 @@ function startGame() {
     console.log(yourSum);
     document.getElementById("hit").addEventListener("click", hit);
     document.getElementById("stay").addEventListener("click", stay);
-  
+
 }
 
 function hit() {
@@ -71,14 +75,14 @@ function hit() {
         return;
     }
 
-    const cardImg = document.createElement("img");
-    const card = deck.pop();
-    cardImg.src = ./cards/${card}.png;
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
     yourSum += getValue(card);
     yourAceCount += checkAce(card);
     document.getElementById("your-cards").append(cardImg);
 
-    if (reduceAce(yourSum, yourAceCount) > 21) { //A, J, 8 -> 1 + 10 + 8
+    if (reduceAce(yourSum, yourAceCount) > 21) { 
         canHit = false;
     }
 
@@ -89,7 +93,7 @@ function stay() {
     yourSum = reduceAce(yourSum, yourAceCount);
 
     canHit = false;
-    document.getElementById("hidden").src = ./cards/${hidden}.png;
+    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
 
     let message = "";
     if (yourSum > 21) {
@@ -98,9 +102,9 @@ function stay() {
     else if (dealerSum > 21) {
         message = "Вы победили!";
     }
-
+    //both you and dealer <= 21
     else if (yourSum == dealerSum) {
-        message = "Ничья!";
+        message = "Ничья";
     }
     else if (yourSum > dealerSum) {
         message = "Вы победили!";
@@ -113,9 +117,10 @@ function stay() {
     document.getElementById("your-sum").innerText = yourSum;
     document.getElementById("results").innerText = message;
 }
+
 function getValue(card) {
-    const data = card.split("-"); // "4-C" -> "4", "C"
-    const value = data0;
+    let data = card.split("-"); 
+    let value = data[0];
 
     if (isNaN(value)) { 
         if (value == "A") {
@@ -127,7 +132,7 @@ function getValue(card) {
 }
 
 function checkAce(card) {
-    if (card0 == "A") {
+    if (card[0] == "A") {
         return 1;
     }
     return 0;
